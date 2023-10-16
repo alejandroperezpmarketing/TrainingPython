a0e6bf9d87f6ad9a505ab76c2ccf536447bded4a
Alejandro Perez <alejandroperezpmarketing@gmail.com>
2023-10-17 00:05:43 +0200

    funcion 2 problema 5 tema 4

--- mi practica python/Tema4_practice_alejandro.py
+++ mi practica python/Tema4_practice_alejandro.py
@@ -41,14 +41,15 @@ def recolectar_inputs_1_2():
     return Valor_1, Valor_2
 
 
-def true_or_false_division_entre_numeros_divisible_o_no(Valor_1,Valor_2):
+def true_or_false_division_entre_numeros_divisible_o_no():
+    recolectar_inputs_1_2()
     if Valor_1%Valor_2 == 0:
         print(True,":","El primer número es divisible por el segundo")
     else:
         print(False,":","El primer número no es divisible por el segundo")
 
 valor1, Valor_2 = recolectar_inputs_1_2()
-true_or_false_division_entre_numeros_divisible_o_no(Valor_1,Valor_2)
+true_or_false_division_entre_numeros_divisible_o_no()
 
 """
 def main_function():
@@ -57,3 +58,30 @@ def main_function():
 
 """
 ##########funcion 2   sumar  todos los datos con ls que se pueda  dividir un valor#########
+#intentar desde el 1 hasta que sea verdadero menos el valor - si un vlor es divisible por n
+#adjuntarlo a la lista, sino dar un print("La suma de los valores divisibles de este numero es:")
+
+
+def calculo_de_suma_de_divisibles():
+    global valores_divisibles
+    valores_divisibles = []
+    n = 1
+    recolectar_inputs_1_2()
+    while True:
+        try:
+            for n in range(n,(valor1-1)):
+                    if Valor_1%n == 0:
+                        valores_divisibles.append(n)
+                        n = n+1
+                    else:
+                        n = n+1
+                        #print("sorry")
+                        pass
+        finally:
+            suma_de_valores_divisibles = sum(valores_divisibles)    
+            print_value = print("La suma de los valores divisibles es:",suma_de_valores_divisibles)
+            return valores_divisibles
+
+
+
+calculo_de_suma_de_divisibles()
